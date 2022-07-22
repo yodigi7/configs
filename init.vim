bc8f08a6a2e1a46f0bb5f7b2b11d2f3e03b97993
yodigi7 <yodigi7@gmail.com>
2022-07-21 21:10:14 -0500

    Converting more to lua and other random stuff

--- init.vim
+++ init.vim
@@ -1,54 +1,4 @@
-call plug#begin()
-
-Plug 'vim-airline/vim-airline' " Status bar
-Plug 'ryanoasis/vim-devicons' " Developer Icons
-Plug 'jiangmiao/auto-pairs' " Auto open and close pairs
-Plug 'liuchengxu/vim-which-key' " Show options for keybindings when in progress
-Plug 'lewis6991/gitsigns.nvim' " Basic additional Git integration with sidebar
-Plug 'tpope/vim-surround' " Surrounding ysw)
-Plug 'tpope/vim-commentary' " For Commenting gcc & gc
-Plug 'tpope/vim-fugitive' " Git integration
-Plug 'tpope/vim-projectionist' " Jump from implementation to test files
-Plug 'tpope/vim-dispatch' " Dispatch built/test/etc jobs to async terminal
-
-Plug 'kana/vim-textobj-entire' " Around everything
-Plug 'kana/vim-textobj-user' " Requirement for around everything
-
-Plug 'preservim/tagbar' " Tagbar for code navigation
-Plug 'ternjs/tern_for_vim', {'do': 'yarn install --frozen-lockfile'} " Requirement for Tagbar
-
-Plug 'xolox/vim-misc' " Requirement for vim notes
-Plug 'xolox/vim-notes' " Note taking
-
-" Telescope
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim', { 'tag': 'nvim-0.6' }
-Plug 'nvim-telescope/telescope-project.nvim'
-
-Plug 'ThePrimeagen/harpoon' "Harpoon
-
-"CoC
-Plug 'neoclide/coc.nvim'  " Auto Completion
-Plug 'pappasam/coc-jedi', { 'do': 'yarn install --frozen-lockfile && yarn build', 'branch': 'main' }
-" TODO
-" Plug 'Lap1n/coc-projector', { 'do': 'yarn install --frozen-lockfile && yarn build' }
-Plug 'yaegassy/coc-pydocstring', {'do': 'yarn install --frozen-lockfile'}
-
-Plug 'voldikss/vim-floaterm' " Floating terminal for reuse
-
-call plug#end()
 lua require('user/config')
-
-" inoremap jk <esc>
-" inoremap kj <esc>
-" nnoremap <CR> o<esc>
-" nnoremap Y y$
-" nnoremap <BS> hx
-
-" Vim settings
-nnoremap <leader>vv :sp $MYVIMRC<CR>
-nnoremap <leader>vo :e $MYVIMRC<CR>
-nnoremap <leader>vs :source $MYVIMRC<CR>
 
 function! TrimWhiteSpace()
   %s/\s*$//
@@ -92,6 +42,7 @@ function! ShowDocumentation()
 endfunction
 " Highlight the symbol and its references when holding the cursor.
 augroup highlightUsages
+    autocmd!
     autocmd CursorHold * silent call CocActionAsync('highlight')
 augroup END
 " Symbol renaming.
@@ -107,64 +58,14 @@ omap ic <Plug>(coc-classobj-i)
 xmap ac <Plug>(coc-classobj-a)
 omap ac <Plug>(coc-classobj-a)
 
-" Coc
-nmap <silent> ] <Plug>(coc-diagnostic-next)
-nmap <silent> [ <Plug>(coc-diagnostic-prev)
-let g:coc_global_extensions = ['coc-snippets', 'coc-explorer', 'coc-tsserver', 'coc-rome', 'coc-pyright', 'coc-json', 'coc-jedi', 'coc-java', 'coc-pydocstring', 'coc-go', 'coc-pairs']
-
-" Coc explorer
-nnoremap <leader>e :CocCommand explorer<CR>
-
 " CoC pydocstring
 nmap <silent> ga <Plug>(coc-codeaction-line)
 xmap <silent> ga <Plug>(coc-codeaction-selected)
 nmap <silent> gA <Plug>(coc-codeaction)
 
-" CoC snippets
-let g:UltiSnipsSnippetDirectories=['~/.config/nvim/ultisnips']
-
-" Telescope
-nnoremap <leader>ff <cmd>Telescope find_files<CR>
-nnoremap <leader>fg <cmd>Telescope live_grep<CR>
-nnoremap <leader>fb <cmd>Telescope buffers<CR>
-map <leader>p <cmd>lua<space>require'telescope'.extensions.project.project{}<CR>
-
-" Fugitive
-nnoremap <leader>gp :Git push<CR>
-nnoremap <leader>gg :Git<CR>
-nnoremap <leader>gs :Git<CR>
-nnoremap <leader>gc :Git commit<CR>
-nnoremap <leader>gb :Git branch<CR>
-nnoremap <leader>grs :Git reset --hard
-nnoremap <leader>go :Git checkout<space>
-nnoremap <leader>ga :Git add<space>
-
-" Which key
-nnoremap <silent> <leader> <cmd>WhichKey '<Space>'<CR>
-
-" Tagbar
-nnoremap <F8> :TagbarToggle fjc<CR>
-
-" Floatterm
-nnoremap <silent> <leader>tt :FloatermToggle<CR>
-nnoremap <silent> <leader>tn :FloatermNew<CR>
-tnoremap <silent> <esc> <C-\><C-n>:FloatermToggle<CR>
-
-" Harpoon
-nnoremap <leader>ha :lua require("harpoon.mark").add_file()<CR>
-nnoremap <silent> <leader>hh :lua require("harpoon.ui").toggle_quick_menu()<CR>
-nnoremap <silent> <leader>ht :lua require("harpoon.ui").toggle_quick_menu()<CR>
-nnoremap <silent> <tab> :lua require("harpoon.ui").nav_next()<CR>
-nnoremap <silent> <S-tab> :lua require("harpoon.ui").nav_prev()<CR>
-
-" Notes
-nnoremap <leader>na :split note:
-nnoremap <leader>nd :DeleteNote<space>
-nnoremap <silent> <leader>nr :RecentNotes<CR>
-nnoremap <silent> <leader>nn :Note standup<CR>
-
 " Dispatch
 augroup dispatch
+    autocmd!
     autocmd FileType java let b:dispatch = 'mvn test'
     autocmd FileType javascript let b:dispatch = 'npm test'
     autocmd FileType python let b:dispatch = 'npm test'
@@ -172,10 +73,4 @@ augroup END
 nnoremap <leader>d :Dispatch<space>
 nnoremap <leader>dd :Dispatch<CR>
 
-" Projectionist
-nnoremap <silent> <leader>a :A<CR>
-" let g:projectionist_heuristics = json_decode(join(readfile(expand('~/.config/projections.json'))))
-
-" Gitsigns
-" nnoremap <leader>gh :Gitsigns stage hunk<CR>
-" vnoremap <leader>gh :Gitsigns stage hunk<CR>
+let g:gruvbox_contrast_dark="soft"
--- lua/user/config.lua
+++ lua/user/config.lua
@@ -1,5 +1,48 @@
 local nvim_set_option = vim.api.nvim_set_option
-local nvim_cmd = vim.api.nvim_command
+
+vim.call('plug#begin')
+local Plug = vim.fn['plug#']
+Plug 'morhetz/gruvbox' -- Gruvbox color scheme
+Plug 'vim-airline/vim-airline' -- Status bar
+Plug 'ryanoasis/vim-devicons' -- Developer Icons
+Plug 'jiangmiao/auto-pairs' -- Auto open and close pairs
+Plug 'liuchengxu/vim-which-key' -- Show options for keybindings when in progress
+Plug 'lewis6991/gitsigns.nvim' -- Basic additional Git integration with sidebar
+Plug 'tpope/vim-surround' -- Surrounding ysw)
+Plug 'tpope/vim-commentary' -- For Commenting gcc & gc
+Plug 'tpope/vim-fugitive' -- Git integration
+Plug 'tpope/vim-projectionist' -- Jump from implementation to test files
+Plug 'tpope/vim-dispatch' -- Dispatch built/test/etc jobs to async terminal
+
+Plug 'kana/vim-textobj-entire' -- Around everything
+Plug 'kana/vim-textobj-user' -- Requirement for around everything
+
+Plug 'preservim/tagbar' -- Tagbar for code navigation
+Plug('ternjs/tern_for_vim', {['do'] = 'yarn install --frozen-lockfile'}) -- Requirement for Tagbar
+
+Plug 'xolox/vim-misc' -- Requirement for vim notes
+Plug 'xolox/vim-notes' -- Note taking
+
+-- Telescope
+Plug 'nvim-lua/plenary.nvim'
+Plug('nvim-telescope/telescope.nvim', { tag = 'nvim-0.6' })
+Plug 'nvim-telescope/telescope-project.nvim'
+
+Plug 'ThePrimeagen/harpoon' --Harpoon
+
+--CoC
+Plug 'neoclide/coc.nvim'  -- Auto Completion
+Plug('pappasam/coc-jedi', { ['do'] = 'yarn install --frozen-lockfile && yarn build', branch = 'main' })
+-- TODO
+-- Plug('Lap1n/coc-projector', {['do'] = 'yarn install --frozen-lockfile && yarn build' })
+Plug('yaegassy/coc-pydocstring', {['do'] = 'yarn install --frozen-lockfile'})
+
+Plug 'voldikss/vim-floaterm' -- Floating terminal for reuse
+
+vim.call('plug#end')
+
+-- Gruvbox
+vim.g.gruvbox_contrast_dark="hard"
 
 -- Functional wrapper for mapping custom keybindings
 function map(mode, lhs, rhs, opts)
@@ -10,7 +53,11 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-nvim_cmd([[
+vim.g.mapleader = " "
+vim.g.maplocalleader = "-"
+vim.g.pymode_python = "python3" -- Using python3
+
+vim.api.nvim_command([[
 set number relativenumber
 set tabstop=4
 set softtabstop=4
@@ -19,31 +66,34 @@ set expandtab
 
 syntax on
 
-set cursorline " Highlight current line
+set cursorline
 
-set lazyredraw " Only redraw once down with macros and whatever
-set nobackup " We have vcs, we don't need backups.
-set nowritebackup " We have vcs, we don't need backups.
+set lazyredraw
+set nobackup
+set nowritebackup
 
-set ignorecase " case insensitive search
-set smartcase " If there are uppercase letters, become case-sensitive.
-set incsearch " live incremental searching
-set showmatch " live match highlighting
-set hlsearch " highlight matches
+set ignorecase
+set smartcase
+set incsearch
+set showmatch
+set hlsearch
 
 set mouse=a
 
 set clipboard=unnamedplus
 
-set autoread " Auto update when something else changes it
+set autoread
 
-set magic " Allows forre common regex
+set magic
 
-set path+=** " Enable :find command to be recursive in the current directory
+set path+=**
 
-set smartindent " Smart indent
+set smartindent
 
-set list listchars=trail:~,extends:> " Show trailing whitespace as ~
+set list listchars=trail:~,extends:>
+
+colorscheme gruvbox
+
 ]])
 
 map("i", "jk", "<esc>")
@@ -53,13 +103,10 @@ map("n", "Y", "y$")
 map("n", "<BS>", "hx")
 
 -- Vimrc settings
-map("n", "<leader>vv", ":split $MYVIMRC<CR>")
-map("n", "<leader>vo", ":edit $MYVIMRC<CR>")
-map("n", "<leader>vs", ":source $MYVIMRC<CR>")
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "-"
-vim.g.pymode_python = "python3" -- Using python3
+map("n", "<leader>vv", ":split $MYVIMRC<CR>", {silent=true})
+map("n", "<leader>vl", ":split ~/.config/nvim/lua/user/config.lua<CR>", {silent=true})
+map("n", "<leader>vo", ":edit $MYVIMRC<CR>", {silent=true})
+map("n", "<leader>vs", ":source $MYVIMRC<CR>", {silent=true})
 
 -- Telescope project
 require'telescope'.load_extension("project")
@@ -71,6 +118,10 @@ telescope.setup {
         }
     }
 }
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {silent=true})
+map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", {silent=true})
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", {silent=true})
+map("", "<leader>p", "<cmd>lua<space>require'telescope'.extensions.project.project{}<CR>", {silent=true})
 
 -- Gitsigns
 require('gitsigns').setup{
@@ -88,3 +139,54 @@ require('gitsigns').setup{
         map('v', '<leader>gh', ':Gitsigns stage_hunk<CR>')
     end
 }
+
+-- Fugitive
+map("n", "<leader>gp", ":Git push<CR>")
+map("n", "<leader>gg", ":Git<CR>", {silent=true})
+map("n", "<leader>gs", ":Git<CR>", {silent=true})
+map("n", "<leader>gc", ":Git commit<CR>", {silent=true})
+map("n", "<leader>gf", ":Git fetch<CR>", {silent=true})
+map("n", "<leader>gb", ":Git branch<CR>", {silent=true})
+map("n", "<leader>grs", ":Git reset --hard")
+map("n", "<leader>go", ":Git checkout<space>")
+map("n", "<leader>ga", ":Git add<space>")
+
+-- Projectionist
+map("n", "<leader>a", ":A<CR>", {silent=true})
+
+-- Notes
+map("n", "<leader>na", ":split note:")
+map("n", "<leader>nd", ":DeleteNote<space>")
+map("n", "<leader>nr", ":RecentNotes<CR>", {silent = true})
+map("n", "<leader>nn", ":split note:standup<CR>", {silent = true})
+
+-- Which key
+map("n", "<leader>", "<cmd>WhichKey '<Space>'<CR>", {silent = true})
+
+-- Harpoon
+map("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>")
+map("n", "<leader>hh", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", {silent=true})
+map("n", "<leader>ht", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", {silent=true})
+map("n", "<tab>", ":lua require('harpoon.ui').nav_next()<CR>", {silent=true})
+map("n", "<S-tab>", ":lua require('qharpoon.ui').nav_prev()<CR>", {silent=true})
+
+-- Floaterm
+map("n", "<leader>tt", ":FloatermToggle<CR>", {silent=true})
+map("n", "<leader>tn", ":FloatermNew<CR>", {silent=true})
+map("t", "<esc>", '<C-\\><C-n>:FloatermToggle<CR>', {silent=true})
+vim.g.floaterm_width = 0.9
+vim.g.floaterm_height = 0.9
+
+-- Tagbar
+map("n", "<F8>", ":TagbarToggle fjc<CR>")
+
+-- CoC
+map("n", "]", "<Plug>(coc-diagnostic-next)", {silent=true})
+map("n", "[", "<Plug>(coc-diagnostic-prev)", {silent=true})
+vim.g.coc_global_extensions = { 'coc-snippets', 'coc-explorer', 'coc-tsserver', 'coc-rome', 'coc-pyright', 'coc-json', 'coc-jedi', 'coc-java', 'coc-pydocstring', 'coc-go', 'coc-pairs', 'coc-markdownlint', 'coc-markdown-preview-enhanced', 'coc-markmap' }
+
+-- Coc explorer
+map("n", "<leader>e", ":CocCommand explorer<CR>")
+
+-- Coc markdown preview
+map("n", "<leader>m", ":CocCommand markdown-preview-enhanced.openPreview<CR>")
